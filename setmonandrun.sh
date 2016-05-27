ca01d4e06266a823dde47824061201c709fa2a10
Robbie Corley <musicmancorley@gmail.com>
2016-05-27 13:58:42 +0000

    adding changes to setmonandrun.sh

--- setmonandrun.sh
+++ setmonandrun.sh
@@ -1,6 +1,6 @@
 echo "Enter your desired monitor interface (eth0, eth1, etc)"
 read -p ":" monint
-ethtool -K $monint tx off rx off sg off gso off gro off
+ethtool -K $monint tx off rx off sg off gso off gro off &
 echo "Making Suricata a background process.  Don't forget to kill it when you are done using it."
 echo "You can kill it by doing the following: ps aux | grep suricata"
 echo "Next, get the PID (process ID) and do this: kill -9 (thePID)"
