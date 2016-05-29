25bdb6e4c6519778137044cba4445c59b47480e9
musicmancorley <musicmancorley@gmail.com>
2016-05-29 18:41:35 +0000

    added zenity to replace message prompts...

--- configcheck.sh
+++ configcheck.sh
@@ -4,7 +4,7 @@ ls /opt/suricata/etc/suricata/suricata.yaml
 if [ $? != 0 ] ; then
    clear
    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-   echo "You need to install BriarIDS first before you check its config..."
+   zenity --info --text="You need to install BriarIDS first before you check its config..."
    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
    sleep 10
    exit
@@ -18,18 +18,17 @@ grep -F '[192.168.0.0/16,10.0.0.0/8,172.16.0.0/12]' /opt/suricata/etc/suricata/s
 if [ $? == 0 ] ; then
    clear
    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-   echo "It looks like you've still got the default config set"
+   zenity --info --text="It looks like you've still got the default config set"
    echo "Optimizing config now... "
    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-   echo "Please enter your WAN ip address below:"
-   read YOUR_WAN_IP
+   YOUR_WAN_IP=$(zenity --entry --entry-text="[WAN_IP]" --text="Please enter your WAN ip address below:")
    sed -i "/172.16.0.0\/12/c\    HOME_NET: \"[$YOUR_WAN_IP\/32,10.0.0.0\/8,192.168.0.0\/16,172.16.0.0\/12]\"" /opt/suricata/etc/suricata/suricata.yaml
    sed -i "/EXTERNAL_NET/c\    EXTERNAL_NET: \"any\"" /opt/suricata/etc/suricata/suricata.yaml
-   echo "ok configuration should be optimized now"
+   zenity --info --text="ok configuration should be optimized now"
    sleep 10
    exit
    else
    clear
-   echo "Your config looks good ;)"
+   zenity --info --text="Your config looks good ;)"
    sleep 10
    fi
