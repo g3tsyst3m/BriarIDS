7aab874e90252caf959a593cc83d6deeed4d0d9a
musicmancorley <musicmancorley@gmail.com>
2016-08-24 19:13:06 +0000

    Added in more dependencies and a check for python-qt4 prior to loading the GUI menu ;)

--- BriarIDS.py
+++ BriarIDS.py
@@ -1,15 +1,20 @@
+import os
 import sys
-from PyQt4 import QtCore, QtGui
+try:
+    from PyQt4 import QtCore, QtGui
+except ImportError:
+    print 'pyqt not installed...installing now.'
+    os.system("sudo apt-get install python-qt4 -y")
+    raise ImportError("ok done installing try running Briar now!")
 ########################################
 #include name of gui python script below
 import loadBriarMenu
 ########################################
-
 class Main(QtGui.QWidget):
     def __init__(self):
         super(Main, self).__init__()
         print "loading main menu..."        
-        
+          
         # name of gui python script
         ############################
         self.ui = loadBriarMenu.Ui_Form()
@@ -17,11 +22,11 @@ class Main(QtGui.QWidget):
         self.ui.setupUi(self)
 
 
-
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     
     app.setStyle("plastique")
     main = Main()
+
     main.show()
     sys.exit(app.exec_())
