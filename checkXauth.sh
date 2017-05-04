619fddedf64902c58e9ec58f067c8c2ca140b260
root <musicmancorley@gmail.com>
2017-05-04 14:54:28 +0000

    updated requested changes to checkauth

--- checkXauth.sh
+++ checkXauth.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-ls /home/pi/.Xauthority > /dev/null
+#ls /home/pi/.Xauthority > /dev/null
 
-if [ "$?" == "0" ]; then
+#if [ "$?" == "0" ]; then
+#sudo cp -f /home/pi/.Xauthority /root/
+#fi
+
+#check for the existence of .Xauthority
+
+ls -ahg /root/.Xauthority &>/dev/null
+#echo $?
+if [ "$?" == "2" ]; then
 sudo cp /home/pi/.Xauthority /root/
 fi
