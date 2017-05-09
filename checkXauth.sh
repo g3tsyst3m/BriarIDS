4058bc763378c395a8d3aa34c6d6c71a387693a6
musicmancorley <musicmancorley@gmail.com>
2017-05-09 02:35:31 +0000

    rollback changes

--- checkXauth.sh
+++ checkXauth.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
 ###########################
-# .Xauthority preparations
-###########################
-
+# .Xauthority craziness
+#
+#
+#rollback the last change as it did not function as expected
+########################################
 ls /etc/profile.d/xauth.sh &>/dev/null
 
-if [ "$?" == "2" ]; then
-cat <<EOF > /etc/profile.d/xauth.sh
-#!/sbin/bash
-export XAUTHORITY=~/.Xauthority
-EOF
+if [ "$?" == "0" ]; then
+sudo rm /etc/profile.d/xauth.sh
 fi
+
+##########################################################################
+#try this for a while until someone forks and replaces w/ more optimal code
+#I opted not to use the $HOME variable as it always assumed it 
+#was /root since ran as sudo
+##########################################################################
+
+
+ls -ahg /home/pi/.Xauthority &>/dev/null
+
+if [ "$?" == "0" ]; then
+sudo cp /home/pi/.Xauthority /root/
+fi
+
 
