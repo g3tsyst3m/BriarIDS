7e0c7fe1675a1a95b6c34fafaca8cf52587f7e8d
musicmancorley <musicmancorley@gmail.com>
2017-05-27 17:25:49 -0400

    trimming unneccessary exploit logs

--- rulecleanup.sh
+++ rulecleanup.sh
@@ -12,12 +12,18 @@ if [ $? != 0 ] ; then
    sed -i '\/bin\/sh/a updatedb' /etc/rc.local
    fi
 
+########################################
+#Removing unnecessary rules
+#
+#Feel free to add your own to this list
+#######################################
+
 grep -qF '# - emerging-chat.rules' /opt/suricata/etc/suricata/suricata.yaml
 if [ $? != 0 ] ; then
    clear
    echo "Commenting out AIM_SERVERS rule..."
    sed -i "/ - emerging-chat.rules/c\# - emerging-chat.rules" /opt/suricata/etc/suricata/suricata.yaml
-   zenity --info --text="Ok.  AIM_SERVERS rule has been corrected!" &> /dev/null
+  #zenity --info --text="Ok.  AIM_SERVERS rule has been corrected!" &> /dev/null
    fi
 
 grep -qF '# - tls-events.rules' /opt/suricata/etc/suricata/suricata.yaml
@@ -25,7 +31,32 @@ if [ $? != 0 ] ; then
    clear
    echo "Commenting out TLS_EVENTS rule..."
    sed -i "/ - tls-events.rules/c\# - tls-events.rules" /opt/suricata/etc/suricata/suricata.yaml
-   zenity --info --text="Ok.  TLS_EVENTS rule has been corrected!" &> /dev/null
+   #zenity --info --text="Ok.  TLS_EVENTS rule has been corrected!" &> /dev/null
    fi
+
+grep -qF '# - stream-events.rules' /opt/suricata/etc/suricata/suricata.yaml
+if [ $? != 0 ] ; then
+   clear
+   echo "Commenting out STREAM_EVENTS rule..."
+   sed -i "/ - stream-events.rules/c\# - stream-events.rules" /opt/suricata/etc/suricata/suricata.yaml
+   #zenity --info --text="Ok.  STREAM_EVENTS rule has been corrected!" &> /dev/null
+   fi
+
+grep -qF '# - app-layer-events.rules' /opt/suricata/etc/suricata/suricata.yaml
+if [ $? != 0 ] ; then
+   clear
+   echo "Commenting out APP_LAYER_EVENTS rule..."
+   sed -i "/ - app-layer-events.rules/c\# - app-layer-events.rules" /opt/suricata/etc/suricata/suricata.yaml
+   #zenity --info --text="Ok.  APP_LAYER_EVENTS rule has been corrected!" &> /dev/null
+   fi
+
+grep -qF '# - decoder-events.rules' /opt/suricata/etc/suricata/suricata.yaml
+if [ $? != 0 ] ; then
+   clear
+   echo "Commenting out DECODER_EVENTS rule..."
+   sed -i "/ - decoder-events.rules/c\# - decoder-events.rules" /opt/suricata/etc/suricata/suricata.yaml
+   #zenity --info --text="Ok.  DECODER_EVENTS rule has been corrected!" &> /dev/null
+   fi
+
    exit
 
