c0ca64cbaf922f875c6fa4a8fdaa3a1abcc343b2
Robbie Corley <musicmancorley@gmail.com>
2018-03-10 09:06:32 -0500

    Update bro-installer.sh

--- scripts/bro-installer.sh
+++ scripts/bro-installer.sh
@@ -8,7 +8,18 @@ cd ~
 echo "Installing prereqs"
 echo "Installing prereqs" >> /usr/local/src/broinstall.log
 sudo apt-get update
-sudo apt-get install cmake make gcc g++ flex bison libcanberra-gtk* libpcap-dev locate libgeoip-dev libssl-dev python-dev zlib1g-dev libmagic-dev swig -y
+sudo apt-get install cmake make gcc g++ flex bison libcanberra-gtk* libpcap-dev locate libgeoip-dev python-dev zlib1g-dev libmagic-dev swig -y
+
+#determine libssl version to install
+ourversion="$(cat /etc/*release)"
+
+if [[ $ourversion == "Debian" ]] ; then
+sudo apt-get install libssl1.0-dev
+else
+sudo apt-get install libssl-dev
+fi
+
+
 sudo apt-get install python-pip -y
 echo "Installing setuptools via pip..." >> /usr/local/src/broinstall.log
 sudo pip install setuptools
